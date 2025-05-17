a42a1f78dc99f2f76fb7a2127d818db143ce47ad
AltheaZuleika <sadiconaltheazuleika@gmail.com>
2025-05-17 19:10:29 +0800

    feat:added an html

--- page/index.php
+++ page/index.php
@@ -48,3 +48,14 @@ function renderChecklist($items){
     return $output;
 }
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Thea's School Routine</title>
+</head>
+<body>
+    
+</body>
+</html>
