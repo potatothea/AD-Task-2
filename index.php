ac1a181c760fa151221b775b97d1dd24661815df
potatothea <202310621@fit.edu.ph>
2025-05-20 00:41:28 +0800

    feat:added the bootstrap php

--- index.php
+++ index.php
@@ -3,15 +3,24 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Thea's School Routine</title>
+    <link rel = "stylesheet" href ="./page/assets/css/css/styles.css">
 </head>
 <body>
-    <div class ="main-container">
-        <div class ="button-wrapper">
-            <a href ="routine.php" class = "arrow-button">View Routine</a>
-        </div>
-    </div>
+    <nav>
+    <div class="logo">MyRoutine</div>
+    <ul>
+      <li><a href="#">Home</a></li>
+      <li><a href="routine.php">Routine</a></li>
+    </ul>
+  </nav>
 
+  <div class="image-section">
+    <img src="home.png" alt="School Routine" />
+    <div class="button-overlay">
+      <a href="routine.php" class="arrow-button">View Routine</a>
+    </div>
+  </div>
     <footer>
         &copy; 2025 Althea Zuleika Sadicon. All rights reserved.
     </footer>
